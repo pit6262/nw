1ee85fa8a85a20534f628b9e6a3254925c861d1d
Pit <orel-1993@yandex.com>
2019-09-10 09:55:50 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,50 @@
+$(window).on('load', function(){
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+		$('html').addClass('ios');
+	};
+	$('body').removeClass('loaded');
+});
+
+$(function(){
+    if($('.reviews').length){
+        $('.reviews').slick({
+        	slidesToShow: 3,
+        	slidesToScroll: 3,
+        	arrows: false,
+        	dots: true,
+        	responsive: [
+        		{
+        			breakpoint: 992, 
+        			settings: {
+        				slidesToShow: 2, 
+        				slidesToScroll: 2,
+        			}
+        		},
+        		{
+        			breakpoint: 767, 
+        			settings: {
+        				slidesToShow: 1, 
+        				slidesToScroll: 1,
+        			}
+        		}
+        	]
+        });
+    };
+
+
+	$('.accordion__head').on('click', function(){
+		var el = $(this);
+		var elNext = $(this).next();
+		$('.accordion__head').not(el).removeClass('open')
+		$('.accordion__body').not(elNext).slideUp(200)
+		el.next('.accordion__body').slideToggle(200);
+		el.toggleClass('open');
+		return false;
+	});
+
+	$('.navbar-toggle').on('click', function(){
+		$(this).toggleClass('is-active');
+		$('.navbar').toggleClass('is-open')
+	})
+
+});
